637e9b452138414279fc3befb8429709167d5c51
M Shoaib <bh01355137@devcorptenant.com>
2025-06-04 22:03:21 +0530

    Fixed 1 SonarQube vulnerabilities

--- src/main/java/com/payrollapplication/EmployeePayrollManagementSystemApplication.java
+++ src/main/java/com/payrollapplication/EmployeePayrollManagementSystemApplication.java
@@ -2,12 +2,17 @@ package com.payrollapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class EmployeePayrollManagementSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollManagementSystemApplication.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(EmployeePayrollManagementSystemApplication.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting Employee Payroll Management System Application");
+        SpringApplication.run(EmployeePayrollManagementSystemApplication.class, args);
+    }
 
 }
